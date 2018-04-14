6e828dafb27cc01b0e65ee32460ff192846cced1
grady407 <grady407@gmail.com>
2018-04-14 11:30:17 -0400

    pleasework

--- client/src/components/pages/FoodFight.js
+++ client/src/components/pages/FoodFight.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const FoodFight = () => (
   <div>
-    <h1>Food Fight</h1>
+    <h1>Topic of discussion is: </h1>
     <p>
    This part is for the food fight!!!!!!
     </p>
